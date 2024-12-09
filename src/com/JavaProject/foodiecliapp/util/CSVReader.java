aaefd6f75b80cf4e0351f8ea96055d75d79f917a
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-09 17:18:24 +0530

    feat : Add CsvReader to read data from csv files

---
+++ src/com/JavaProject/foodiecliapp/util/CSVReader.java
@@ -0,0 +1,93 @@
+package com.JavaProject.foodiecliapp.util;
+
+import com.JavaProject.foodiecliapp.model.Customer;
+import com.JavaProject.foodiecliapp.model.Dish;
+import com.JavaProject.foodiecliapp.model.Restaurant;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CSVReader {
+
+    public List<Customer> readCustomersFromCsv(){
+
+        String customerFilePath = "C:\\Users\\naras\\OneDrive\\Desktop\\Full stack java\\JavaWorkSpace\\food-ordering-cli-app\\data\\customers.csv";
+
+        List<Customer> customerList = new ArrayList<>();
+        String line;
+        String csvSplitBy = ",";
+        try(BufferedReader br = new BufferedReader(new FileReader(customerFilePath))){
+            br.readLine();
+            while ((line = br.readLine()) != null){
+                String[] data = line.split(csvSplitBy);
+                //id,name,email,password
+                Customer customer = new Customer();
+                customer.setId(data[0]);
+                customer.setName(data[1]);
+                customer.setEmail(data[2]);
+                customer.setPassword(data[3]);
+                customerList.add(customer);
+            }
+        } catch (IOException e) {
+            System.out.println("File not found in the path :"+customerFilePath);
+            System.exit(0);
+            System.out.println(e.getMessage());
+        }
+        return customerList;
+    }
+
+    public List<Dish> readDishesFromCsv(){
+        String DishesFilePath ="C:\\Users\\naras\\OneDrive\\Desktop\\Full stack java\\JavaWorkSpace\\food-ordering-cli-app\\data\\dishes.csv";
+
+        List<Dish> dishList = new ArrayList<>();
+        String line;
+        String csvSplitBy = ",";
+        try(BufferedReader br = new BufferedReader(new FileReader(DishesFilePath))){
+            br.readLine();
+            while((line = br.readLine()) != null){
+                String[] data = line.split(csvSplitBy);
+                Dish dish = new Dish();
+                dish.setId(data[0]);
+                dish.setName(data[1]);
+                dish.setDescription(data[2]);
+                dish.setPrice(Double.parseDouble(data[3]));
+                dishList.add(dish);
+            }
+        } catch (IOException e) {
+            System.out.println("File not found in the path :"+DishesFilePath);
+            System.exit(0);
+            System.out.println(e.getMessage());
+        }
+        return  dishList;
+    }
+
+    public  List<Restaurant>  readRestaurantsFromCsv(){
+        String RestaurantCsvFilePath ="C:\\Users\\naras\\OneDrive\\Desktop\\Full stack java\\JavaWorkSpace\\food-ordering-cli-app\\data\\restaurants.csv";
+        List<Restaurant> restaurantList = new ArrayList<>();
+        String Line;
+        String CsvSplitBy = ",";
+        try(BufferedReader br = new BufferedReader(new FileReader(RestaurantCsvFilePath))){
+            br.readLine();
+            while ((Line = br.readLine()) != null){
+                String[] data = Line.split(CsvSplitBy);
+                Restaurant restaurant = new Restaurant();
+                restaurant.setId(data[0]);
+                restaurant.setName(data[1]);
+                restaurant.setAddress(data[2]);
+                restaurant.setMenu(Arrays.asList(data[3].split(":")));
+                restaurantList.add(restaurant);
+            }
+
+        } catch (IOException e) {
+            System.out.println("File not found in this path:"+RestaurantCsvFilePath);
+            System.exit(0);
+            System.out.println(e.getMessage());
+        }
+        return restaurantList;
+    }
+}
