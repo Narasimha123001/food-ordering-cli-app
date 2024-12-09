165bb2643c5234df9507632bb5b98e03f3f0ac98
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-09 22:45:27 +0530

    feat: Implement CustomerService interface in CustomerServiceImpl class

---
+++ src/com/JavaProject/foodiecliapp/service/CustomerServiceImpl.java
@@ -0,0 +1,26 @@
+package com.JavaProject.foodiecliapp.service;
+
+import com.JavaProject.foodiecliapp.exceptions.CustomerExitsException;
+import com.JavaProject.foodiecliapp.model.Customer;
+import com.JavaProject.foodiecliapp.repository.CustomerRepository;
+
+import java.util.Optional;
+
+public class CustomerServiceImpl implements CustomerService{
+
+    private CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public Customer save(Customer customer) throws CustomerExitsException {
+        Optional<Customer> customerById=this.customerRepository.findCustomerById(customer.getId());
+        if(customerById.isPresent())
+            throw new CustomerExitsException("Id is already present"+customer.getId()+" please try with different id ");
+
+        return this.customerRepository.save(customer);
+    }
+
+}
