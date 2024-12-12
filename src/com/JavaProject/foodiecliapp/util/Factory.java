2cf55ba23770acdce259a82cac399696ace06638
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-12 18:53:30 +0530

    feat: Add Factory class to initialize RestaurantRepository, RestaurantService, and RestaurantController

--- src/com/JavaProject/foodiecliapp/util/Factory.java
+++ src/com/JavaProject/foodiecliapp/util/Factory.java
@@ -1,8 +1,11 @@
 package com.JavaProject.foodiecliapp.util;
 
 import com.JavaProject.foodiecliapp.controller.CustomerController;
+import com.JavaProject.foodiecliapp.controller.RestaurantController;
 import com.JavaProject.foodiecliapp.repository.CustomerRepository;
+import com.JavaProject.foodiecliapp.repository.RestaurantRepository;
 import com.JavaProject.foodiecliapp.service.CustomerServiceImpl;
+import com.JavaProject.foodiecliapp.service.RestaurantServiceImpl;
 
 public class Factory {
 
@@ -16,5 +19,17 @@ public class Factory {
 
     public static CustomerController customerController(){
         return new CustomerController(customerService());
+    }
+
+    public static RestaurantRepository restaurantRepository(){
+        return new RestaurantRepository();
+    }
+
+    public static RestaurantServiceImpl restaurantService(){
+        return new RestaurantServiceImpl(restaurantRepository());
+    }
+
+    public static RestaurantController restaurantController(){
+       return new RestaurantController(restaurantService());
     }
 }
