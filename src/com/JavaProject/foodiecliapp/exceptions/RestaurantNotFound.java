a5491f4a987b0ff3fcb3990e6d3da4ed473ba2ca
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-12 18:49:39 +0530

    feat: Add RestaurantNotFound.java class

---
+++ src/com/JavaProject/foodiecliapp/exceptions/RestaurantNotFound.java
@@ -0,0 +1,7 @@
+package com.JavaProject.foodiecliapp.exceptions;
+
+public class RestaurantNotFound  extends Exception{
+    public RestaurantNotFound(String message) {
+        super(message);
+    }
+}
