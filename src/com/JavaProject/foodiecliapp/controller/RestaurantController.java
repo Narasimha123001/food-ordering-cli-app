77642696485a4453bc7b4c8c3bbb437eeb7ececd
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-12 18:48:48 +0530

    feat: Add RestaurantController class with save method

---
+++ src/com/JavaProject/foodiecliapp/controller/RestaurantController.java
@@ -0,0 +1,25 @@
+package com.JavaProject.foodiecliapp.controller;
+
+import com.JavaProject.foodiecliapp.exceptions.RestaurantAlreadyExistsException;
+import com.JavaProject.foodiecliapp.model.Restaurant;
+import com.JavaProject.foodiecliapp.service.RestaurantServiceImpl;
+
+import java.util.List;
+
+public class RestaurantController {
+
+    private final RestaurantServiceImpl restaurantService;
+
+    public RestaurantController(RestaurantServiceImpl restaurantService) {
+        this.restaurantService = restaurantService;
+    }
+
+    public Restaurant save(Restaurant restaurant) throws RestaurantAlreadyExistsException {
+        return this.restaurantService.save(restaurant);
+    }
+
+    public List<Restaurant> getRestaurantList(){
+        return this.restaurantService.getRestaurantList();
+    }
+
+}
