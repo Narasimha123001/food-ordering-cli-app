4813a2f1b1dff4cdc8d8c099a8837ac5dc2621af
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-12 18:50:43 +0530

    feat: Add Factory class to initialize RestaurantRepository, RestaurantService, and RestaurantController

---
+++ src/com/JavaProject/foodiecliapp/Main.java
@@ -0,0 +1,10 @@
+package com.JavaProject.foodiecliapp;
+
+import com.JavaProject.foodiecliapp.ui.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.displayMainMenu();
+    }
+}
