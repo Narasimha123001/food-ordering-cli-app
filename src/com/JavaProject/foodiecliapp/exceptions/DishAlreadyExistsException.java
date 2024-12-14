8c30d757200a4ca955d7be7e7b7ab9157420b6c5
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-14 21:55:27 +0530

    feat: Add DishAlreadyExistsException for handling duplicate dish entries

---
+++ src/com/JavaProject/foodiecliapp/exceptions/DishAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.JavaProject.foodiecliapp.exceptions;
+
+public class DishAlreadyExistsException extends Exception{
+    public DishAlreadyExistsException(String message) {
+        super(message);
+    }
+}
