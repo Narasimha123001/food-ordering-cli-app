091b4ec784a3ca72a31d499bc9bf9ee2341c34d6
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-15 00:02:35 +0530

    feat: optimize the code for CustomerRepository

--- src/com/JavaProject/foodiecliapp/repository/CustomerRepository.java
+++ src/com/JavaProject/foodiecliapp/repository/CustomerRepository.java
@@ -2,6 +2,7 @@ package com.JavaProject.foodiecliapp.repository;
 
 import com.JavaProject.foodiecliapp.model.Customer;
 import com.JavaProject.foodiecliapp.util.CSVReader;
+import com.JavaProject.foodiecliapp.util.Factory;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,8 +12,7 @@ public class CustomerRepository {
     private final List<Customer> customerList;
 
     public CustomerRepository() {
-        CSVReader csvReader = new CSVReader();
-        this.customerList = csvReader.readCustomersFromCsv();
+        this.customerList = Factory.getCsvReader().readCustomersFromCsv();
     }
 
     public List<Customer> getAllCustomerList(){
