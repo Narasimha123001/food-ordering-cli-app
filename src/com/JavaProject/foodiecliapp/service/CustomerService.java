20c86cbe9354c78cbbac86d5e172f92e9786ec3d
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-14 21:47:42 +0530

    feat: Add getAll customers feature in CustomerService

--- src/com/JavaProject/foodiecliapp/service/CustomerService.java
+++ src/com/JavaProject/foodiecliapp/service/CustomerService.java
@@ -3,8 +3,12 @@ package com.JavaProject.foodiecliapp.service;
 import com.JavaProject.foodiecliapp.exceptions.CustomerExitsException;
 import com.JavaProject.foodiecliapp.model.Customer;
 
+import java.util.List;
+
 public interface CustomerService {
 
     public Customer save(Customer customer) throws CustomerExitsException;
+
+    public List<Customer> getAllCustomerList();
 
 }
