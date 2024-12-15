c28e5b9752cc95e9a6372a306c12d87c7a422776
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-16 02:06:11 +0530

    refactor: Rename CustomerExistsException to CustomerAlreadyExistsException

--- src/com/JavaProject/foodiecliapp/exceptions/CustomerExitsException.java
+++
@@ -1,7 +0,0 @@
-package com.JavaProject.foodiecliapp.exceptions;
-
-public class CustomerExitsException extends Exception{
-    public CustomerExitsException(String message) {
-        super(message);
-    }
-}
