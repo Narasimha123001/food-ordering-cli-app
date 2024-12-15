8bb967c9ea3aca817e1d0ea383e3e8c5fd129779
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-16 02:04:09 +0530

    feat: Add CustomerAlreadyExistsException for handling duplicate customer entries

---
+++ src/com/JavaProject/foodiecliapp/exceptions/CustomerAlreadyExitsException.java
@@ -0,0 +1,7 @@
+package com.JavaProject.foodiecliapp.exceptions;
+
+public class CustomerAlreadyExitsException extends Exception{
+    public CustomerAlreadyExitsException(String message) {
+        super(message);
+    }
+}
