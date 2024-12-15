0392f1fd67ee0905b95d7d1801507c7131d17ac2
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-16 02:10:07 +0530

    Add updateCustomerDetails and deleteCustomer methods in CustomerService

--- src/com/JavaProject/foodiecliapp/service/CustomerService.java
+++ src/com/JavaProject/foodiecliapp/service/CustomerService.java
@@ -1,6 +1,6 @@
 package com.JavaProject.foodiecliapp.service;
 
-import com.JavaProject.foodiecliapp.exceptions.CustomerExitsException;
+import com.JavaProject.foodiecliapp.exceptions.CustomerAlreadyExitsException;
 import com.JavaProject.foodiecliapp.exceptions.CustomerNotFoundException;
 import com.JavaProject.foodiecliapp.model.Customer;
 
@@ -8,10 +8,13 @@ import java.util.List;
 
 public interface CustomerService {
 
-    public Customer save(Customer customer) throws CustomerExitsException;
+    public Customer save(Customer customer) throws CustomerAlreadyExitsException;
 
     public List<Customer> getAllCustomerList();
 
     public Customer getCustomerById(String id)throws CustomerNotFoundException;
 
+    public Customer updateCustomer(Customer customerToBeUpdate) throws CustomerNotFoundException;
+
+    public void deleteCustomer(String id) throws CustomerNotFoundException;
 }
