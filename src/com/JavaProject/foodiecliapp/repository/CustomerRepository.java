9a01a45a1e7f50b76ff2ab295f79b91f5a0239af
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-16 02:07:28 +0530

    feat: Add updateCustomer and deleteCustomer methods in CustomerRepository

--- src/com/JavaProject/foodiecliapp/repository/CustomerRepository.java
+++ src/com/JavaProject/foodiecliapp/repository/CustomerRepository.java
@@ -1,7 +1,6 @@
 package com.JavaProject.foodiecliapp.repository;
 
 import com.JavaProject.foodiecliapp.model.Customer;
-import com.JavaProject.foodiecliapp.util.CSVReader;
 import com.JavaProject.foodiecliapp.util.Factory;
 
 import java.util.List;
@@ -10,6 +9,7 @@ import java.util.Optional;
 public class CustomerRepository {
 
     private final List<Customer> customerList;
+    private final String customerFilePath ="C:\\Users\\naras\\OneDrive\\Desktop\\Full stack java\\JavaWorkSpace\\food-ordering-cli-app\\data\\customers.csv";
 
     public CustomerRepository() {
         this.customerList = Factory.getCsvReader().readCustomersFromCsv();
@@ -21,6 +21,7 @@ public class CustomerRepository {
         //adding new Customer
     public Customer save(Customer customer){
         this.customerList.add(customer);
+        Factory.getCsvWriter().writeCustomerToCsv(customerList ,customerFilePath );
         return customer;
     }
 
@@ -28,4 +29,22 @@ public class CustomerRepository {
         return this.customerList.stream()
                 .filter(customer -> customer.getId().equals(id)).findFirst();
     }
- }
+
+    public Customer updateCustomer(Customer customerToBeUpdate){
+        Optional<Customer> optionalCustomer = this.customerList.stream()
+                .filter(customer -> customer.getId().equals(customerToBeUpdate.getId()))
+                .findFirst()
+                .map(customer -> {
+                    customer.setName(customerToBeUpdate.getName())
+                            .setEmail(customerToBeUpdate.getEmail())
+                            .setPassword(customerToBeUpdate.getPassword());
+                    Factory.getCsvWriter().writeCustomerToCsv(customerList, customerFilePath);
+                    return customer;
+                });
+        return optionalCustomer.orElse(null);
+    }
+
+    public void deleteCustomer(Customer customer){
+         this.customerList.remove(customer);
+    }
+}
