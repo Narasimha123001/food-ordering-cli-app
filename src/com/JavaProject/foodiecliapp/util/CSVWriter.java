044f411d36a893cf32fabd32e7e63b9400dc385d
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-16 02:01:43 +0530

    feat: Add CSVWriter to handle writing data to CSV files

---
+++ src/com/JavaProject/foodiecliapp/util/CSVWriter.java
@@ -0,0 +1,24 @@
+package com.JavaProject.foodiecliapp.util;
+
+import com.JavaProject.foodiecliapp.model.Customer;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class CSVWriter {
+    public void writeCustomerToCsv(List<Customer> customers, String customerFilePath) {
+        try(FileWriter writer = new FileWriter(customerFilePath)){
+            for(Customer customer: customers){
+                writer.write(String.format("%s,%s,%s,%s\n",
+                        customer.getId(),
+                        customer.getName(),
+                        customer.getEmail(),
+                        customer.getPassword()));
+            }
+        } catch (IOException e) {
+            System.out.println("Error occurs! while saving the customer in the file path"+customerFilePath);
+            System.out.println(e.getMessage());
+        }
+    }
+}
