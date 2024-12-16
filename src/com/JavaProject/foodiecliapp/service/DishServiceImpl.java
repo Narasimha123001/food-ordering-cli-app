1528035f6f815750c3ae9481f85acce35c0fd656
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-17 01:25:10 +0530

    feat: Implement update, get, and delete methods in DishServiceImpl

--- src/com/JavaProject/foodiecliapp/service/DishServiceImpl.java
+++ src/com/JavaProject/foodiecliapp/service/DishServiceImpl.java
@@ -1,6 +1,7 @@
 package com.JavaProject.foodiecliapp.service;
 
 import com.JavaProject.foodiecliapp.exceptions.DishAlreadyExistsException;
+import com.JavaProject.foodiecliapp.exceptions.DishesNotFoundException;
 import com.JavaProject.foodiecliapp.model.Dish;
 import com.JavaProject.foodiecliapp.repository.DishRepository;
 
@@ -31,5 +32,27 @@ public class DishServiceImpl implements DishService {
         return this.dishRepository.getDishList();
     }
 
+    @Override
+    public Dish updateDish(Dish dishToBeUpdated) throws DishesNotFoundException {
+        Optional<Dish> optionalDish = this.dishRepository.getDishById(dishToBeUpdated.getId());
+        if(optionalDish.isEmpty())
+            throw new DishesNotFoundException("Dish not found based on provided Id: "+dishToBeUpdated.getId()+". please try with correct id");
+        return this.dishRepository.updateDish(dishToBeUpdated);
+    }
 
+    @Override
+    public Dish getDishById(String id) throws DishesNotFoundException {
+        Optional<Dish> optionalDish = this.dishRepository.getDishById(id);
+        if(optionalDish.isEmpty())
+            throw  new DishesNotFoundException("Dish not found by this id: "+id+"please try with correct id");
+        return optionalDish.get();
+    }
+
+    @Override
+    public void deleteDish(String id) throws DishesNotFoundException {
+        Optional<Dish> optionalDish = this.dishRepository.getDishById(id);
+        if(optionalDish.isEmpty())
+            throw new DishesNotFoundException("Dish not found by this id: "+id+"please try with correct id");
+        this.dishRepository.deleteDish(getDishById(id));
+    }
 }
