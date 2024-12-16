40f078b9854a033d70aeb8c83a5c02ff73f2c4a2
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-17 01:24:28 +0530

    feat: Add update, get, and delete methods in DishService interface

--- src/com/JavaProject/foodiecliapp/service/DishService.java
+++ src/com/JavaProject/foodiecliapp/service/DishService.java
@@ -1,6 +1,7 @@
 package com.JavaProject.foodiecliapp.service;
 
 import com.JavaProject.foodiecliapp.exceptions.DishAlreadyExistsException;
+import com.JavaProject.foodiecliapp.exceptions.DishesNotFoundException;
 import com.JavaProject.foodiecliapp.model.Dish;
 
 import java.util.List;
@@ -10,4 +11,10 @@ public interface DishService {
     public Dish save(Dish dish) throws DishAlreadyExistsException;
 
     public  List<Dish> getDishList();
+
+    public Dish updateDish(Dish dishToBeUpdated) throws DishesNotFoundException;
+
+    public Dish getDishById(String id) throws DishesNotFoundException;
+
+    public void deleteDish(String id) throws DishesNotFoundException;
 }
