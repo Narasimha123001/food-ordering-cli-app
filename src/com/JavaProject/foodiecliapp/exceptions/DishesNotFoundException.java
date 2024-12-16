22a43c51b6d65122a2cedbb0fe65362204d772ba
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-17 01:28:14 +0530

    feat: Add DishesNotFoundException for handling missing dish scenarios

---
+++ src/com/JavaProject/foodiecliapp/exceptions/DishesNotFoundException.java
@@ -0,0 +1,7 @@
+package com.JavaProject.foodiecliapp.exceptions;
+
+public class DishesNotFoundException extends Exception{
+    public DishesNotFoundException(String message) {
+        super(message);
+    }
+}
