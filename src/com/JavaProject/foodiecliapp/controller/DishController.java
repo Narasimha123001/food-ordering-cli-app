b56edde8928fc68367cc5f2691f408c982d43f03
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-17 01:25:54 +0530

    feat: Add update, get, and delete methods in DishController

--- src/com/JavaProject/foodiecliapp/controller/DishController.java
+++ src/com/JavaProject/foodiecliapp/controller/DishController.java
@@ -1,6 +1,7 @@
 package com.JavaProject.foodiecliapp.controller;
 
 import com.JavaProject.foodiecliapp.exceptions.DishAlreadyExistsException;
+import com.JavaProject.foodiecliapp.exceptions.DishesNotFoundException;
 import com.JavaProject.foodiecliapp.model.Dish;
 import com.JavaProject.foodiecliapp.service.DishServiceImpl;
 
@@ -21,4 +22,13 @@ public class DishController {
         return this.dishService.getDishList();
     }
 
+    public Dish updateDish(Dish dishToBeUpdated) throws DishesNotFoundException{
+        return this.dishService.updateDish(dishToBeUpdated);
+    }
+    public Dish getDishById(String id) throws DishesNotFoundException{
+        return this.dishService.getDishById(id);
+    }
+    public void deleteDish(String id) throws DishesNotFoundException{
+        this.dishService.deleteDish(id);
+    }
 }
