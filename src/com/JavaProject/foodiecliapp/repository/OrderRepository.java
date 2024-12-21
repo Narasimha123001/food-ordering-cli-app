46e957fc6d450e07188668c6cd33deb2748e1e6c
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-22 01:33:12 +0530

    feat: Add CRUD operations and validations for order management

---
+++ src/com/JavaProject/foodiecliapp/repository/OrderRepository.java
@@ -0,0 +1,28 @@
+package com.JavaProject.foodiecliapp.repository;
+
+import com.JavaProject.foodiecliapp.model.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class OrderRepository {
+    private final List<Order> orderList;
+
+    public OrderRepository() {
+        this.orderList = new ArrayList<>();
+    }
+
+    public Order save(Order order){
+        this.orderList.add(order);
+        return order;
+    }
+
+    public List<Order> getOrderList(){
+        return  this.orderList;
+    }
+
+    public Optional<Order> findOrderById(String id){
+        return this.orderList.stream().filter(order -> order.getId().equals(id)).findFirst();
+    }
+}
