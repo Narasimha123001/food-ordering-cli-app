d94c72abddefd8e38b8f15f23b044eec35824adf
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-24 00:13:29 +0530

    refactor: Refactor Factory to use static initialization holder pattern.

--- src/com/JavaProject/foodiecliapp/util/Factory.java
+++ src/com/JavaProject/foodiecliapp/util/Factory.java
@@ -12,58 +12,74 @@ import com.JavaProject.foodiecliapp.service.CustomerServiceImpl;
 import com.JavaProject.foodiecliapp.service.DishServiceImpl;
 import com.JavaProject.foodiecliapp.service.OrderServiceImpl;
 import com.JavaProject.foodiecliapp.service.RestaurantServiceImpl;
-
 public class Factory {
 
-    public static CSVReader getCsvReader(){
-        return new CSVReader();
+    private static final class Holder {
+        private static final CSVReader CSV_READER = new CSVReader();
+        private static final CustomerRepository CUSTOMER_REPOSITORY = new CustomerRepository();
+        private static final CustomerServiceImpl CUSTOMER_SERVICE = new CustomerServiceImpl(CUSTOMER_REPOSITORY);
+        private static final CustomerController CUSTOMER_CONTROLLER = new CustomerController(CUSTOMER_SERVICE);
+        private static final DishRepository DISH_REPOSITORY = new DishRepository();
+        private static final DishServiceImpl DISH_SERVICE = new DishServiceImpl(DISH_REPOSITORY);
+        private static final DishController DISH_CONTROLLER = new DishController(DISH_SERVICE);
+        private static final RestaurantRepository RESTAURANT_REPOSITORY = new RestaurantRepository();
+        private static final RestaurantServiceImpl RESTAURANT_SERVICE = new RestaurantServiceImpl(RESTAURANT_REPOSITORY);
+        private static final RestaurantController RESTAURANT_CONTROLLER = new RestaurantController(RESTAURANT_SERVICE);
+        private static final OrderRepository ORDER_REPOSITORY = new OrderRepository();
+        private static final OrderServiceImpl ORDER_SERVICE = new OrderServiceImpl(ORDER_REPOSITORY);
+        private static final OrderController ORDER_CONTROLLER = new OrderController(ORDER_SERVICE);
     }
 
-    public static CSVWriter getCsvWriter(){
-        return new CSVWriter();
-    }
-    public static CustomerRepository customerRepository(){
-        return new CustomerRepository();
+    // Static method access provides the same functionality with reduced boilerplate
+    public static CSVReader csvReader() {
+        return Holder.CSV_READER;
     }
 
-    public static CustomerServiceImpl customerService(){
-        return new CustomerServiceImpl(customerRepository());
+    public static CustomerRepository customerRepository() {
+        return Holder.CUSTOMER_REPOSITORY;
     }
 
-    public static CustomerController customerController(){
-        return new CustomerController(customerService());
+    public static CustomerServiceImpl customerService() {
+        return Holder.CUSTOMER_SERVICE;
     }
 
-    public static RestaurantRepository restaurantRepository(){
-        return new RestaurantRepository();
+    public static CustomerController customerController() {
+        return Holder.CUSTOMER_CONTROLLER;
     }
 
-    public static RestaurantServiceImpl restaurantService(){
-        return new RestaurantServiceImpl(restaurantRepository());
+    public static DishRepository dishRepository() {
+        return Holder.DISH_REPOSITORY;
     }
 
-    public static RestaurantController restaurantController(){
-       return new RestaurantController(restaurantService());
+    public static DishServiceImpl dishService() {
+        return Holder.DISH_SERVICE;
     }
 
-    public static DishRepository dishRepository(){
-        return new DishRepository();
-    }
-    public static DishServiceImpl dishService(){
-        return new DishServiceImpl(dishRepository());
-    }
-    public static DishController dishController(){
-        return new DishController(dishService());
+    public static DishController dishController() {
+        return Holder.DISH_CONTROLLER;
     }
 
-    public static OrderRepository orderRepository(){
-        return new OrderRepository();
-    }
-    public static OrderServiceImpl orderService(){
-        return new OrderServiceImpl(orderRepository());
-    }
-    public static OrderController orderController(){
-        return new OrderController(orderService());
+    public static RestaurantRepository restaurantRepository() {
+        return Holder.RESTAURANT_REPOSITORY;
     }
 
+    public static RestaurantServiceImpl restaurantService() {
+        return Holder.RESTAURANT_SERVICE;
+    }
+
+    public static RestaurantController restaurantController() {
+        return Holder.RESTAURANT_CONTROLLER;
+    }
+
+    public static OrderRepository orderRepository() {
+        return Holder.ORDER_REPOSITORY;
+    }
+
+    public static OrderServiceImpl orderService() {
+        return Holder.ORDER_SERVICE;
+    }
+
+    public static OrderController orderController() {
+        return Holder.ORDER_CONTROLLER;
+    }
 }
