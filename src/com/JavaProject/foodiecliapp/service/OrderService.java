485d8811f3e1dc054065f4cd204d81e6837bcbdd
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-24 12:29:23 +0530

    refactor: Rename method findOrderById to getOrderById in OrderService

--- src/com/JavaProject/foodiecliapp/service/OrderService.java
+++ src/com/JavaProject/foodiecliapp/service/OrderService.java
@@ -5,12 +5,11 @@ import com.JavaProject.foodiecliapp.exceptions.OrderNotFoundException;
 import com.JavaProject.foodiecliapp.model.Order;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface OrderService {
     public Order save(Order order) throws OrderExistsExceptions;
 
     public List<Order> getOrderList();
 
-    public Order findOrderById(String id) throws OrderNotFoundException;
+    public Order getOrderById(String id) throws OrderNotFoundException;
 }
