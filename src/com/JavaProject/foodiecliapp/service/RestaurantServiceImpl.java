d0c468a91ae251f2651300eaef44927a83349e8a
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-24 12:33:26 +0530

    feat: Add getDishItems method to fetch dishes for a restaurant

--- src/com/JavaProject/foodiecliapp/service/RestaurantServiceImpl.java
+++ src/com/JavaProject/foodiecliapp/service/RestaurantServiceImpl.java
@@ -1,10 +1,14 @@
 package com.JavaProject.foodiecliapp.service;
 
+import com.JavaProject.foodiecliapp.exceptions.DishesNotFoundException;
 import com.JavaProject.foodiecliapp.exceptions.RestaurantAlreadyExistsException;
 import com.JavaProject.foodiecliapp.exceptions.RestaurantNotFound;
+import com.JavaProject.foodiecliapp.model.Dish;
 import com.JavaProject.foodiecliapp.model.Restaurant;
 import com.JavaProject.foodiecliapp.repository.RestaurantRepository;
+import com.JavaProject.foodiecliapp.util.Factory;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,8 +22,8 @@ public class RestaurantServiceImpl implements RestaurantService{
     @Override
     public Restaurant saveRestaurant(Restaurant restaurant) throws RestaurantAlreadyExistsException {
        Optional<Restaurant> optionalRestaurant = this.restaurantRepository.findRestaurantById(restaurant.getId());
-       if(optionalRestaurant.isEmpty())
-           throw new RestaurantAlreadyExistsException("Restaurant already present");
+       if(optionalRestaurant.isPresent())
+           throw new RestaurantAlreadyExistsException("Restaurant already present on same Id. Please try again with another id");
        return this.restaurantRepository.saveRestaurant(restaurant);
     }
 
@@ -51,4 +55,21 @@ public class RestaurantServiceImpl implements RestaurantService{
             throw new RestaurantNotFound("Restaurant Not found by id"+id);
         this.restaurantRepository.deleteRestaurant(getRestaurantById(id));
     }
+
+    @Override
+    public List<Dish> getDishItems(String id) throws RestaurantNotFound, DishesNotFoundException {
+        Optional<Restaurant> restaurantById = this.restaurantRepository.findRestaurantById(id);
+        if(restaurantById.isEmpty())
+            throw new RestaurantNotFound("Restaurant Not Found with this id:"+id);
+        List<Dish> dishList = new ArrayList<>();
+        Restaurant restaurant = restaurantById.get();
+        List<String> dishIds = restaurant.getMenu();
+        DishService dishService = Factory.dishService();
+        for(String dishId : dishIds){
+            Dish dish = dishService.getDishById(dishId);
+            dishList.add(dish);
+        }
+        return dishList;
+    }
+
 }
